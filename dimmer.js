1bcc4c44a974130def68639431d0f9752cf9d4a6
Kostas Minaidis <minaidisk@gmail.com>
2016-01-09 13:25:47 +0100

    Update dimmer.js

--- dimmer.js
+++ dimmer.js
@@ -2,10 +2,18 @@
 
     var styleEl = document.querySelector("style#dim-fb")
     if ( styleEl === null ){
+        var rules = "#pagelet_bluebar,#pagelet_sidebar,#rightCol,#leftCol{opacity:0.2;}";
         var s=document.createElement("style");
-        s.setAttribute("id","dim-fb");
-        s.innerText="#pagelet_bluebar,#pagelet_sidebar,#rightCol,#leftCol{opacity:0.2;}";
-        document.body.appendChild(s);
+            s.type = 'text/css';
+            s.setAttribute("id","dim-fb");
+        if (s.styleSheet){
+          s.styleSheet.cssText = rules;
+        } else {
+          s.appendChild(document.createTextNode(rules));
+
+        }
+        var head = document.head || document.getElementsByTagName('head')[0];
+            head.appendChild(s);
     } else {
         styleEl.remove();
     }
